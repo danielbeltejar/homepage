017e58483bc5c53bd129ca41e68c5034ff9ed1c1
Daniel Beltejar <danielbeltejar@gmail.com>
2023-04-01 15:02:10 +0200

    Updated title link animation

--- assets/scripts/hover.js
+++ assets/scripts/hover.js
@@ -1,15 +1,12 @@
 const projectDiv = document.getElementsByClassName("project");
 const projectsDiv = document.getElementById("projects");
-const projectsTitleBorder = document.getElementById("bg-light");
 
 for (let i = 0; i < projectDiv.length; i++) {
     projectDiv[i].addEventListener("mouseover", () => {
         projectsDiv.style.backgroundColor = "var(--dark-color)";
-        projectsTitleBorder.style.borderBottom = "3px solid var(--dark-color)";
     });
     
     projectDiv[i].addEventListener("mouseout", () => {
         projectsDiv.style.backgroundColor = "var(--light-color)";
-        projectsTitleBorder.style.borderBottom = "3px solid var(--light-color)";
     });
 }
