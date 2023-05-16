c337e5885bfa19bc2fdb1e86c2e2495bcd54a522
Daniel Beltejar <danielbeltejar@gmail.com>
2023-05-16 15:49:32 +0200

    Changed button height on hover.

--- assets/scripts/hover.js
+++ assets/scripts/hover.js
@@ -1,12 +1,29 @@
 const projectDiv = document.getElementsByClassName("project");
 const projectsDiv = document.getElementById("projects");
 
+
 for (let i = 0; i < projectDiv.length; i++) {
     projectDiv[i].addEventListener("mouseover", () => {
         projectsDiv.style.backgroundColor = "var(--dark-color)";
+
+
+        buttons = projectDiv[i].getElementsByClassName("button");
+        for (let j = 0; j < buttons.length; j++) {
+            width = buttons[j].offsetWidth;
+            buttons[j].style.height = 97;
+            buttons[j].style.width = 97;
+
+          }
+      
     });
     
     projectDiv[i].addEventListener("mouseout", () => {
         projectsDiv.style.backgroundColor = "var(--light-color)";
+
+        buttons = projectDiv[i].getElementsByClassName("button");
+        for (let j = 0; j < buttons.length; j++) {
+            buttons[j].style.height = null;
+            buttons[j].style.width = null;
+          }
     });
 }
