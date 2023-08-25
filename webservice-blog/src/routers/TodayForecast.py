2d334319a21b2aaeed9f54958b649e90fea6b500
Daniel Beltejar <danielbeltejar@gmail.com>
2023-08-25 14:50:29 +0200

    Added web service for latest blog entry.

--- webservice-blog/src/routers/TodayForecast.py
+++ webservice-blog/src/routers/TodayForecast.py
@@ -1,14 +1,17 @@
-import os
 import logging
-from datetime import datetime
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi import status
-from fastapi.responses import StreamingResponse
+
+from src.worker.LatestBlogEntry import get_latest_wordpress_blog_entry
 
 latest_blog_entry = APIRouter()
 logger = logging.getLogger()
 
 
-@latest_blog_entry.get('/blog/latest', status_code=status.HTTP_200_OK)
+@latest_blog_entry.get('/latest', status_code=status.HTTP_200_OK)
 def latest_blog_entry_router():
+    latest_entry = get_latest_wordpress_blog_entry()
+    if "error" in latest_entry:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=latest_entry["error"])
+    return latest_entry
--- webservice-blog/src/worker/LatestBlogEntry.py
+++ webservice-blog/src/worker/LatestBlogEntry.py
@@ -2,35 +2,30 @@ import requests
 
 
 def get_latest_wordpress_blog_entry():
-    # Define the base URL of your WordPress site's REST API
     base_url = "https://blog.danielbeltejar.es/wp-json/wp/v2"
 
-    # Define the endpoint for retrieving the latest post
     endpoint = "/posts?per_page=1&_embed"
 
-    # Make the GET request to the API
     response = requests.get(base_url + endpoint)
 
     if response.status_code == 200:
-        # Parse the JSON response
         data = response.json()
 
         if data:
-            # Extract the latest blog post's title and content
             latest_post = data[0]
-            post_title = latest_post["title"]["rendered"]
-            post_content = latest_post["content"]["rendered"]
+            post_url = latest_post["link"]
+
+            post_title:str = latest_post["title"]["rendered"]
+            if post_title.__contains__(" – "):
+                post_title = post_title.split(" – ")[1]
+            elif post_title.__contains__(" - "):
+                post_title = post_title.split(" - ")[1]
 
             return {
                 "title": post_title,
-                "content": post_content
+                "url": post_url
             }
         else:
             return {"error": "No blog posts found."}
     else:
         return {"error": "Failed to retrieve blog post."}
-
-
-# Test the function
-latest_blog_entry = get_latest_wordpress_blog_entry()
-print(latest_blog_entry)
