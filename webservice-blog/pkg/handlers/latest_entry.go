dda56142647f22a3bfb944992fdcac155cf30925
Daniel Beltejar <danielbeltejar@gmail.com>
2023-08-28 13:19:27 +0200

    Fixed wordpress title crawling.

--- webservice-blog/pkg/handlers/latest_entry.go
+++ webservice-blog/pkg/handlers/latest_entry.go
@@ -19,14 +19,12 @@ func LatestBlogEntryHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Parse the original JSON response into a slice of maps
 	var posts []map[string]interface{}
 	if err := json.Unmarshal(jsonResponse, &posts); err != nil {
 		http.Error(w, "Error parsing JSON response", http.StatusInternalServerError)
 		return
 	}
 
-	// Check if there is at least one post
 	if len(posts) == 0 {
 		http.Error(w, "No posts found in the response", http.StatusNotFound)
 		return
@@ -34,28 +32,24 @@ func LatestBlogEntryHandler(w http.ResponseWriter, r *http.Request) {
 
 	title := posts[0]["title"].(map[string]interface{})["rendered"].(string)
 	if strings.Contains(title, "-") {
-		title = strings.Split(title, " - ")[0]
+		title = strings.Split(title, " - ")[1]
 	} else if strings.Contains(title, " – ") {
-		title = strings.Split(title, " – ")[0]
+		title = strings.Split(title, " – ")[1]
 	}
 
-	// Extract and format the desired data
 	entry := BlogEntry{
 		Title: title,
 		URL:   posts[0]["link"].(string),
 	}
 
-	// Marshal the simplified JSON object
 	simplifiedJSON, err := json.Marshal(entry)
 	if err != nil {
 		http.Error(w, "Error creating simplified JSON response", http.StatusInternalServerError)
 		return
 	}
 
-	// Set the Content-Type header to indicate JSON response
 	w.Header().Set("Content-Type", "application/json")
 
-	// Write the simplified JSON response to the response writer
 	_, err = w.Write(simplifiedJSON)
 	if err != nil {
 		http.Error(w, "Error writing JSON response", http.StatusInternalServerError)
--- webservice-blog/pkg/wordpress/wordpress.go
+++ webservice-blog/pkg/wordpress/wordpress.go
@@ -7,10 +7,8 @@ import (
 )
 
 func GetLatestWordpressBlogEntry() ([]byte, error) {
-	// URL of the WordPress site's REST API endpoint for posts
 	apiUrl := "https://blog.danielbeltejar.es/wp-json/wp/v2/posts?per_page=1&orderby=date"
 
-	// Send an HTTP GET request to the API endpoint
 	response, err := http.Get(apiUrl)
 	if err != nil {
 		return nil, err
@@ -21,19 +19,16 @@ func GetLatestWordpressBlogEntry() ([]byte, error) {
 		return nil, fmt.Errorf("Request failed with status code: %s", response.Status)
 	}
 
-	// Parse the JSON response into a slice of generic maps
 	var posts []map[string]interface{}
 	decoder := json.NewDecoder(response.Body)
 	if err := decoder.Decode(&posts); err != nil {
 		return nil, err
 	}
 
-	// Check if there is at least one post
 	if len(posts) == 0 {
 		return nil, fmt.Errorf("No posts found in the response")
 	}
 
-	// Convert the result to JSON
 	jsonResponse, err := json.Marshal(posts)
 	if err != nil {
 		return nil, err
