5c69706285739417578a35d3d8d9ffde592ee951
Daniel Beltejar <danielbeltejar@gmail.com>
2023-08-28 13:15:20 +0200

    Added request logging.

--- webservice-blog/cmd/main.go
+++ webservice-blog/cmd/main.go
@@ -10,11 +10,21 @@ import (
 
 func main() {
 	r := mux.NewRouter()
+	r.Use(LoggerMiddleware)
 	r.HandleFunc("/latest", handlers.LatestBlogEntryHandler).Methods("GET")
 	r.HandleFunc("/healthz", handlers.HealthHandler).Methods("GET")
+	http.Handle("/", r)
 
 	http.Handle("/", r)
 
 	fmt.Println("Listening on :8080...")
 	http.ListenAndServe(":8080", nil)
+}
+
+func LoggerMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Request URL: %s\n", r.URL.Path)
+
+		next.ServeHTTP(w, r)
+	})
 }
