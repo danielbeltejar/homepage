1bc5a2817c31e8238964626b6b37e13e7374229f
Daniel Beltejar <danielbeltejar@gmail.com>
2025-01-26 22:22:59 +0100

    migrated to vite

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,14 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import CompressionPlugin from 'vite-plugin-compression';  // Correct import
 
 export default defineConfig({
-    plugins: [react(), ],
+    plugins: [
+        react(),
+        CompressionPlugin({
+            algorithm: 'brotliCompress',
+        }),
+    ],
     server: {
         port: 3000,
         open: true,
     },
     build: {
         outDir: 'build',
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['react', 'react-dom'],  // Example: Split libraries
+                }
+            }
+        }
     },
     esbuild: {
         loader: 'tsx',
