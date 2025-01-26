8604ddcb4c752b0cbda8a7a50062a7fcf5a8c685
Daniel Beltejar <danielbeltejar@gmail.com>
2025-01-26 23:58:59 +0100

    added new post banner

--- front/src/components/Header.ts
+++ front/src/components/Header.ts
@@ -8,7 +8,7 @@ const Header = () => {
     useEffect(() => {
         async function fetchData() {
             try {
-                const response = await fetch('http://127.0.0.1:8000/posts/newest');
+                const response = await fetch('https://danielbeltejar.es/api/posts/newest');
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
