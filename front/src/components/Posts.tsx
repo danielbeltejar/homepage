37c9a3c72a6f0dd06bb57ca56c89a526a5557454
Daniel Beltejar <danielbeltejar@gmail.com>
2025-02-22 00:25:10 +0100

    updated to add posts page

--- front/src/components/Posts.tsx
+++ front/src/components/Posts.tsx
@@ -46,22 +46,34 @@ export default function Posts() {
 
     return (
         <div className='bg-window dark:bg-dark-window mt-16 mb-16 p-10 shadow-lg min-h-[800px]'>
+            <div className="flex flex-row align-middle items-center mt-xs group w-full">
+                <a href="/posts"
+                    className="text-accent dark:text-dark-accent opacity-0 absolute group-hover:opacity-100 transition-opacity duration-300">
+                    <FontAwesomeIcon icon={faLink} />
+                </a>
+                <h2 className="text-accent dark:text-dark-accent font-bold text-2xl group-hover:cursor-pointer group-hover:pl-7 group-hover:underline underline-offset-4 transition-all duration-300"
+                    onClick="location.href='/posts';">Posts</h2>
+            </div>
+
+            <p className="mt-4 mb-10 text-12">
+                Welcome to my blog! Here, I share insights into my personal projects, hobby projects, and various topics related to my career. Stay tuned for updates and feel free to explore the posts below.
+            </p>
             <div className="flex flex-col align-middle items-start mt-xs group w-ful gap-y-6">
                 {currentPosts.map((post) => (
                     <PostCard
                         key={post.filename}
                         title={post.title}
                         date={post.date}
-                        content={post.content} 
+                        content={post.content}
                         onClick={() => navigate(`/post/${post.filename.replace('.md', '')}`)}
                     />
                 ))}
                 <div className="pagination">
                     {Array.from({ length: Math.ceil(posts.length / postsPerPage) }, (_, index) => (
-                        <Button 
-                            key={index + 1} 
-                            text={(index + 1).toString()} 
-                            onClick={() => paginate(index + 1)} 
+                        <Button
+                            key={index + 1}
+                            text={(index + 1).toString()}
+                            onClick={() => paginate(index + 1)}
                         />
                     ))}
                 </div>
