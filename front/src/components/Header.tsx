e5fb9392d71739a29ebb7bf495098e5a5447591d
Daniel Beltejar <danielbeltejar@gmail.com>
2025-03-03 17:19:44 +0100

    update

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -34,9 +34,9 @@ const Header = () => {
             </Link>
             <Link id="blog-link" style={{ opacity: blogUrl ? 1 : 0 }}
                 to={blogUrl}
-                className="transition-opacity ease-in-out duration-1000 text-window dark:text-dark-text m-0 lg:max-w-[350px] max-w-full">
+                className="transition-opacity ease-in-out duration-1000 text-window dark:text-dark-text m-0 lg:max-w-[375px] max-w-full">
                 <div
-                    className="text-12 flex gap-2 font-bold bg-accent dark:bg-dark-accent overflow-hidden  px-1 flex-row align-middle items-center rounded-full h-[33px] m-0">
+                    className="text-12 flex gap-2 font-bold bg-accent dark:bg-dark-accent overflow-hidden drop-shadow-lg shadow-lg px-1 flex-row align-middle items-center rounded-full h-[33px] m-0">
                     <p className="border-2  border-window dark:border-dark-text text-[0.8rem] p-[0.5px] px-2 rounded-full mt-[1px] m-0">Blog</p>
                     <p className="text-sm text-ellipsis overflow-hidden whitespace-nowrap mr-1">{blogTitle}</p>
                 </div>
