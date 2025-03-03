b14acaac2ac0e3603f8365bf3b3637db7a07fde9
Daniel Beltejar <danielbeltejar@gmail.com>
2025-03-03 17:12:34 +0100

    update

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
                     throw new Error('Network response was not ok');
                 }
                 const data = await response.json();
-                const url = `${window.location.origin}/post/${data.filename.replace('.md', '')}`;
+                const url = `/post/${data.filename.replace('.md', '')}`;
                 setBlogUrl(url);
                 setBlogTitle(data.title);
             } catch (error) {
@@ -25,7 +25,6 @@ const Header = () => {
         fetchData();
     }, []);
 
-
     return (
         <header className="flex flex-row w-full bg-window dark:bg-dark-window p-10 justify-center lg:pt-12 lg:pb-8 items-center">
             <Link id="title-effect" style={{ opacity: 0 }}
@@ -33,16 +32,15 @@ const Header = () => {
                 className="transition-opacity text-3xl ease-in-out duration-700 font-bold text-accent dark:text-dark-accent flex-row align-middle justify-center items-center w-fit mr-auto hidden lg:block">
                 <h1>Daniel Beltejar</h1>
             </Link>
-            <a target="_blank" id="blog-link" style={{ opacity: blogUrl ? 1 : 0 }}
-                href={blogUrl}
-                rel="noreferrer"
+            <Link id="blog-link" style={{ opacity: blogUrl ? 1 : 0 }}
+                to={blogUrl}
                 className="transition-opacity ease-in-out duration-1000 text-window dark:text-dark-text m-0 lg:max-w-[350px] max-w-full">
                 <div
                     className="text-12 flex gap-2 font-bold bg-accent dark:bg-dark-accent overflow-hidden  px-1 flex-row align-middle items-center rounded-full h-[33px] m-0">
                     <p className="border-2  border-window dark:border-dark-text text-[0.8rem] p-[0.5px] px-2 rounded-full mt-[1px] m-0">Blog</p>
                     <p className="text-sm text-ellipsis overflow-hidden whitespace-nowrap mr-1">{blogTitle}</p>
                 </div>
-            </a>
+            </Link>
         </header>
     );
 };
